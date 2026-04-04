6cd2334f7395683ded11db3882176d3717502479
Gavin Noronha <gavinln@hotmail.com>
2026-04-04 14:22:21 -0700

    add examples of declaring variables in javascript

--- javascript-fundamentals/variables.js
+++ javascript-fundamentals/variables.js
@@ -4,3 +4,13 @@ let message;
 message = "Hello!";
 
 console.log(message);
+
+// declare multiple variables in one statement
+let _user = "John",
+  _age = 25,
+  _greeting = "Hello";
+
+let hello = "Hello world!";
+
+// copy 'Hello world' from hello into message
+message = hello;
