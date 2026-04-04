29b5a9d506d88030570aed87e8a7ebf1f6705e22
Gavin Noronha <gavinln@hotmail.com>
2026-04-04 14:55:45 -0700

    add more variable declarations

--- javascript-fundamentals/variables.js
+++ javascript-fundamentals/variables.js
@@ -14,3 +14,23 @@ let hello = "Hello world!";
 
 // copy 'Hello world' from hello into message
 message = hello;
+
+let $ = 1;
+
+let _ = 2;
+
+console.log($ + _);
+
+const _myBirthday = "18.04.1982";
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+console.log(color); // #FF7F00
+console.log(COLOR_RED);
+console.log(COLOR_GREEN);
+console.log(COLOR_BLUE);
